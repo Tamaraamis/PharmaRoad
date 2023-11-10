a1d742594dd23121b967e145585601ea1b506a09
Zainrafati <zynalrfatygmail.com>
2023-11-10 20:39:10 +0300

    zain

---
+++ lib/home/zain.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Zain extends StatefulWidget {
+  const Zain({super.key});
+
+  @override
+  State<Zain> createState() => _ZainState();
+}
+
+class _ZainState extends State<Zain> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
