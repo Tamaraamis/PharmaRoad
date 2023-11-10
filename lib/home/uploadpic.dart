664e7ab5bc7877800896437e540de439d3b1c690
tamaraamis002@gmail.com <79707234+Tamaraamis@users.noreply.github.com>
2023-11-10 19:06:34 +0300

    pic

--- lib/home/uploadpic.dart
+++ lib/home/uploadpic.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class uploadpic extends StatefulWidget {
   const uploadpic({super.key});
