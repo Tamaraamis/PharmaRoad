4273655b27ebbc8704a68dc17c55b60cfb68f5e3
Zainrafati <zynalrfatygmail.com>
2024-01-01 18:14:31 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@ import 'package:flutter_application_2/PharmacyManger/current.dart';
 import 'package:flutter_application_2/PharmacyManger/homeph.dart';
 import 'package:flutter_application_2/PharmacyManger/profileM.dart';
 import 'package:flutter_application_2/auth/welcome.dart';
-import 'package:flutter_application_2/home/UserProfile.dart';
+//import 'package:flutter_application_2/home/UserProfile.dart';
 import 'package:flutter_application_2/home/YourPersc.dart';
 import 'package:flutter_application_2/home/contact-us.dart';
 import 'package:flutter_application_2/home/profilee.dart';
+
 import 'package:flutter_application_2/home/searchph.dart';
 import 'package:flutter_application_2/poplularmedicine/product.dart';
 import 'PharmacyManger/list.dart';
@@ -18,7 +19,7 @@ import 'auth/signup.dart';
 import 'auth/users.dart';
 import 'home/searche.dart';
 import 'home/try.dart';
-import 'pharmacyloc/pharmacyloc.dart';
+//import 'pharmacyloc/pharmacyloc.dart';
 import 'pharmacyloc/region1.dart';
 import 'pharmacyloc/region2.dart';
 import 'pharmacyloc/region3.dart';
@@ -28,6 +29,7 @@ import 'pharmacyloc/region6.dart';
 import 'pharmacyloc/region7.dart';
 import 'pharmacyloc/region8.dart';
 import 'home/uploadpic.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
@@ -49,41 +51,59 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FirebaseAuth.instance.currentUser == null ? const splach() :  const drawer(),
-      theme: ThemeData(primaryColor: const Color.fromRGBO(115, 144, 215, 0.86)),
-      routes: {
-        "Signup": (context) => const Signup(),
-        "Users": (context) => const Users(),
-        "Searchdrug": (context) => const SearchDrug(),
-        "Welcome": (context) => const Welcome(),
-        "drawer": (context) => const drawer(),
-        "contact": (context) => const Contact(),
-        "LoginManger": (context) => const LogM(),
-        //"homeph": (context) => const LogM(),
-        //"drug":(context) => Drug(),
-        "list": (context) => Medicine(),
-        "try": (context) => Log(),
-        "pharmacyloc": (context) => pharmacyloc(),
-        "region1": (context) => region1(),
-        "region2": (context) => region2(),
-        "region3": (context) => region3(),
-        "region4": (context) => region4(),
-        "region5": (context) => region5(),
-        "region6": (context) => region6(),
-        "region7": (context) => region7(),
-        "region8": (context) => region8(),
-        "uploadpic": (context) => uploadpic(),
-        //"contactD": (context) => contactd(),
-        "product":(context) => product(),
-        "contact-us":(context) => contactR(),
-        "profilee":(context) => profile(),
-        "profileM":(context) => pro(),
-        "YourPersc":(context) => const perscShow(),
-        "UserProfile":(context) => const UserProfile(),
-        "searchph":(context) => const SearchPharmacy(),
 
+        debugShowCheckedModeBanner: false,
+        home: FirebaseAuth.instance.currentUser == null
+            ? const splach()
+            : const drawer(),
+        theme:
+            ThemeData(primaryColor: const Color.fromRGBO(115, 144, 215, 0.86)),
+        routes: {
+          "Signup": (context) => const Signup(),
+          "Users": (context) => const Users(),
+          "Searchdrug": (context) => const SearchDrug(),
+          "Welcome": (context) => const Welcome(),
+          "drawer": (context) => const drawer(),
+          "contact": (context) => const Contact(),
+          "LoginManger": (context) => const LogM(),
+          //"homeph": (context) => const LogM(),
+          //"drug":(context) => Drug(),
+          "list": (context) => const Medicine(),
+          "try": (context) => const Log(),
+          "pharmacyloc": (context) => const SearchPharmacy(),
+          "region1": (context) => const region1(),
+          "region2": (context) => const region2(),
+          "region3": (context) => const region3(),
+          "region4": (context) => const region4(),
+          "region5": (context) => const region5(),
+          "region6": (context) => const region6(),
+          "region7": (context) => const region7(),
+          "region8": (context) => const region8(),
+          "uploadpic": (context) => const uploadpic(),
+          "contact-us": (context) => const contactR(),
+          "profilee": (context) => profile(),
+          "product": (context) => const product(),
+          "profileM": (context) => const pro(),
+          "YourPersc": (context) => const perscShow(),
+          'homeph': (context) {
+            final Map<String, dynamic> args = ModalRoute.of(context)!
+                .settings
+                .arguments as Map<String, dynamic>;
+            return Homeph(pharmacyId: args['id']);
+          },
+          'current': (context) {
+            final Map<String, dynamic> args = ModalRoute.of(context)!
+                .settings
+                .arguments as Map<String, dynamic>;
+            return PharmacyMedicinesPage(pharmacyId: args['id']);
+          },
+          /* "homeph": (context) {
+  final Map<String, dynamic>? args = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
 
+  return Homeph(pharmacyId: args?['id'] ?? '');
+},
+
+       
       },
       onGenerateRoute: (settings) {
         if (settings.name == 'current') {
